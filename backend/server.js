aabcc751804cc087d7862ccae07a3918adf59218
Meherdip Pract <mrdhokane@gmail.com>
2019-01-07 23:42:02 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 import express from 'express';
- 
+import cors from 'cors'; 
+import BodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 const app=express();
-app.get('/',(req,res)=>res.send('Hello world!'));
+const router = express.Router();
+app.use(cors);
+app.use(BodyParser.json());
+mongoose.connect('');
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log('connection is esablish');
+})
+app.use('/'.router);
+
+
+//app.get('/',(req,res)=>res.send('Hello world!'));
 app.listen(4000,()=>console.log('Express runnig on 4000port'));
